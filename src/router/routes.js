f5cb3e8152b4fa37dce787a75d9bb4414e0a2de9
xxx <xxx@xxx.xxx>
2026-02-11 14:57:11 +0100

    x

--- src/router/routes.js
+++ src/router/routes.js
@@ -9,6 +9,9 @@ const routes = [
       }
     ]
   }, 
+  
+  // Always leave this as last one,
+  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
