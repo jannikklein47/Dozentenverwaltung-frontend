5ed9f86e44f98d5eafd75cb2326f935651806510
Pier-tech22 <peer.ganzer@gmail.com>
2026-04-14 23:17:40 +0200

    Update ChangeInitialPasswordPage.vue

--- src/pages/ChangeInitialPasswordPage.vue
+++ src/pages/ChangeInitialPasswordPage.vue
@@ -17,28 +17,49 @@
         autocomplete="email"
         :disable="loading"
       />
-      <q-input
-        label="Neues Passwort"
-        outlined
-        rounded
-        class="q-mb-md"
-        type="password"
-        v-model="password"
-        autocomplete="password"
-        :disable="loading"
-      />
-      <q-input
-        label="Neues Passwort bestätigen"
-        outlined
-        rounded
-        class="q-mb-md"
-        type="password"
-        v-model="repeatPassword"
-        autocomplete="repeatpassword"
-        :disable="loading"
-        :error="repeatPassword !== password"
-        error-message="Die Passwörter müssen übereinstimmen."
-      />
+<q-input
+  label="Neues Passwort"
+  outlined
+  rounded
+  class="q-mb-md"
+  :type="showPassword ? 'text' : 'password'"
+  v-model="password"
+  autocomplete="password"
+  :disable="loading"
+>
+  <template v-slot:append>
+    <q-icon
+      :name="showPassword ? 'visibility_off' : 'visibility'"
+      class="cursor-pointer"
+      @click="showPassword = !showPassword"
+    />
+  </template>
+</q-input>
+
+<q-input
+  label="Neues Passwort bestätigen"
+  outlined
+  rounded
+  class="q-mb-md"
+  :type="showRepeatPassword ? 'text' : 'password'"
+  v-model="repeatPassword"
+  autocomplete="repeatpassword"
+  :disable="loading"
+  :error="repeatPassword !== password"
+  error-message="Die Passwörter müssen übereinstimmen."
+>
+  <template v-slot:append>
+    <q-icon
+      :name="showRepeatPassword ? 'visibility_off' : 'visibility'"
+      class="cursor-pointer"
+      @click="showRepeatPassword = !showRepeatPassword"
+    />
+  </template>
+</q-input>
+
+<div class="text-caption text-grey-7 q-mb-md">
+  {{ passwordRulesText }}
+</div>
       <q-btn
         label="Bestätigen"
         class="q-mb-md full-width bg-primary text-white q-py-sm"
@@ -64,7 +85,13 @@ const router = useRouter()
 const password = ref('')
 const repeatPassword = ref('')
 const loading = ref(false)
+const showPassword = ref(false)
+const showRepeatPassword = ref(false)
 
+const passwordRulesText =
+  'Das Passwort muss mindestens 8 Zeichen lang sein und mindestens einen Großbuchstaben, einen Kleinbuchstaben, eine Zahl und ein Sonderzeichen enthalten.'
+
+  
 async function login() {
   loading.value = true
   try {
